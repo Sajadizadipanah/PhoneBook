f0ec5526b10835abf358b993453ae1e1f14cad05
Sajadizadipanah <138392702+Sajadizadipanah@users.noreply.github.com>
2024-07-23 02:41:04 +0330

    .

--- MyApplication/obj/Debug/net6.0-windows/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net6.0-windows/MyApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ed8186d4820707dad19fc7a7611ea9474f44b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
